6f1e40e0a0d77a83f2072bad83f51e20c74df2c3
Justin Laureano <jmlaureano87@gmail.com>
2025-03-30 18:23:48 -0400

    added some chakra

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+"use client"
+
 import { Button, HStack } from "@chakra-ui/react"
+import { useColorMode } from "@/components/ui/color-mode"
 
 export default function Home() {
+	const { toggleColorMode } = useColorMode()
+
 	return (
 		<HStack>
-			<Button>See Posts</Button>
-			<Button>Create Post</Button>
+			<Button onClick={toggleColorMode}>Toggle Color Mode</Button>
 		</HStack>
 	);
 }
